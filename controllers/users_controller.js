8e439a72008370f62db71708b5ee44f8681694e4
Trung Pham <phamanhtrung@gmail.com>
2020-07-08 11:34:15 -0500

    set up mongodb

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -5,17 +5,19 @@ const User = require("../models/users.js");
 const { response } = require("express");
 
 users.get("/new", (req, res) => {
-  res.render("../views/login.ejs");
+  res.render("users/new.ejs");
 });
 
 users.post("/", (req, res) => {
+  console.log("flag");
+  console.log(req.body);
   req.body.password = bcrypt.hashSync(
     req.body.password,
     bcrypt.genSaltSync(10)
   );
   User.create(req.body, (err, createdUser) => {
     console.log("user is created", createdUser);
-    res.redirect("/");
+    res.redirect("/photos");
   });
 });
 
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 4000;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/photo_users", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
+});
+
+app.use(express.json());
+app.use(express.urlencoded());
 
 const userController = require("./controllers/users_controller.js");
 app.use("/users", userController);
