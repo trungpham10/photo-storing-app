d159c320afd600dd73a16f6d51bc89034ad0a1c4
Trung Pham <phamanhtrung@gmail.com>
2020-07-09 01:11:52 -0500

    add login authorization

--- controllers/sessions_controller.js
+++ controllers/sessions_controller.js
@@ -7,4 +7,22 @@ sessions.get("/new", (req, res) => {
   res.render("sessions/new.ejs", { currentUser: req.session.currentUser });
 });
 
+sessions.post("/", (req, res) => {
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+      res.send("oops the db had a problem");
+    } else if (!foundUser) {
+      res.send("<a href='/photos'>Sorry, no user found</a>");
+    } else {
+      if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+        req.session.currentUser = foundUser;
+        res.redirect("/photos");
+      } else {
+        res.send('<a href="/photos"> password does not match </a>');
+      }
+    }
+  });
+});
+
 module.exports = sessions;
