8f904f12194275be7d4d21702aefa6b5076da716
Trung Pham <phamanhtrung@gmail.com>
2020-07-12 17:52:32 -0500

    fix controller bug

--- controllers/moments_controller.js
+++ controllers/moments_controller.js
@@ -12,49 +12,62 @@ moments.get("/add", (req, res) => {
   res.render("add.ejs", { currentUser: req.session.currentUser });
 });
 
-moments.post("/add", (req, res) => {
+moments.post("/", (req, res) => {
   User.findById(req.body.userID, (err, foundUser) => {
     Moment.create(req.body, (err, createdMoment) => {
-      if (err) {
-        console.log(err);
-        res.send(err);
-      } else {
-        console.log("moment created: ", createdMoment);
-        foundUser.moments.push(createdMoment);
-        foundUser.save((err, data) => {
-          res.render("/");
-        });
-      }
+      console.log("moment created: ", createdMoment);
+      foundUser.moments.push(createdMoment);
+      foundUser.save((err, data) => {
+        res.redirect("/");
+      });
     });
   });
 });
 
 moments.get("/:id", (req, res) => {
   Moment.findById(req.params.id, (err, foundMoment) => {
-    res.render("show.ejs", {
-      moment: foundMoment
+    User.findOne({ "moments._id": req.params.id }, (err, foundUser) => {
+      res.render("show.ejs", {
+        moment: foundMoment,
+        user: foundUser,
+      });
     });
-  })
+  });
 });
 
 moments.get("/:id/edit", (req, res) => {
   Moment.findById(req.params.id, (err, foundMoment) => {
     res.render("edit.ejs", {
       moment: foundMoment,
-      currentUser: req.session.currentUser
+      currentUser: req.session.currentUser,
     });
-  })
-})
+  });
+});
 
 moments.put("/:id", (req, res) => {
-  console.log(req.body);
   Moment.findByIdAndUpdate(
     req.params.id,
     req.body,
+    { new: true },
     (err, updatedMoment) => {
-      res.redirect(`/${req.params.id}`)
+      User.findOne({ "moments._id": req.params.id }, (err, foundUser) => {
+        foundUser.moments.id(req.params.id).remove();
+        foundUser.moments.push(updatedMoment);
+        res.redirect(`/${req.params.id}`);
+      });
     }
-  )
-})
+  );
+});
+
+moments.delete("/:id", (req, res) => {
+  Moment.findByIdAndRemove(req.params.id, (err, foundMoment) => {
+    User.findOne({ "moments._id": req.params.id }, (err, foundUser) => {
+      foundUser.moments.id(req.params.id).remove();
+      foundUser.save((err, data) => {
+        res.redirect("/");
+      });
+    });
+  });
+});
 
 module.exports = moments;
